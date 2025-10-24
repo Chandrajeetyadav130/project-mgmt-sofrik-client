f40fc4c18fa50985a07f3a4c180e2ba546ac5fff
Chandrajeetyadav130 <chandrajeetyadav130@gmail.com>
2025-10-24 16:18:44 +0530

    add validation in login and registration

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,24 +4,49 @@ import { login } from '../actions/authAction';
 import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
-  const [email, setEmail] = useState('test@example.com');
-  const [password, setPassword] = useState('Test@123');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error,setError]=useState({})
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const validate=()=>{
+    const newError={}
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!email){
+      newError.email="email is required"
+    }
+        else if(!emailRegex.test(email)){
 
+    }
+    if(!password){
+      newError.password="Password is required"
+    }
+    return newError
+  }
   const submit = async e => {
     e.preventDefault();
-    await dispatch(login(email, password));
-    alert("Login successful")
-    navigate('/');
+    const validateErrors=validate()
+     setError(validateErrors)
+    if(Object.keys(validateErrors).length===0){
+      try {
+            await dispatch(login(email, password));
+         alert("Login successful")
+         navigate('/');
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
   };
 
   return (
     <div className="reg-logincontainer">
       <form className="login_reg_form" onSubmit={submit}>
         <h2>Login</h2>
-        <input className='login_reg_input' value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
-        <input className='login_reg_input' value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" type="password" />
+        <input className='login_reg_input' value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" autoComplete="off" />
+       {error.email && <p style={{ color: 'red' }}>{error.email}</p>}
+        <input className='login_reg_input' value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" type="password" autoComplete="off" />
+       {error.password && <p style={{ color: 'red' }}>{error.password}</p>}
         <button className='login-reg-btn' type="submit">Login</button>
         <p>or <Link to="/register">Register</Link></p>
       </form>
--- src/components/Register.js
+++ src/components/Register.js
@@ -7,21 +7,57 @@ export default function Register() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState({})
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  // validation error logic
+  const validate = () => {
+    let newErrors = {}
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!name) {
+      newErrors.name = "Name is required"
+    }
+    if (!email) {
+      newErrors.email = "Email is required"
+    }
+    else if (!emailRegex.test(email)) {
 
-  const submit = async e => { e.preventDefault(); 
-    await dispatch(register(name, email, password)); 
-    alert("registration successful")
-    navigate('/'); };
+    }
+    if (!password) {
+      newErrors.password = "Password is required"
+    }
+    return newErrors
+  }
+  const submit = async e => {
+    e.preventDefault();
+    const validationError = validate()
+    setError(validationError)
+    if (Object.keys(validationError).length === 0) {
+      try {
+        await dispatch(register(name, email, password));
+        alert("registration successful")
+        navigate('/')
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+
+  }
 
   return (
     <div className="reg-logincontainer">
       <form className="login_reg_form" onSubmit={submit}>
         <h2>Register</h2>
         <input className='login_reg_input' value={name} onChange={e => setName(e.target.value)} placeholder="Name" />
+        {error.name && <p style={{ color: 'red' }}>{error.name}</p>}
         <input className='login_reg_input' value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
+        {error.email && <p style={{ color: 'red' }}>{error.email}</p>}
+
         <input className='login_reg_input' value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" type="password" />
+        {error.password && <p style={{ color: 'red' }}>{error.password}</p>}
+
         <button className='login-reg-btn' type="submit">Register</button>
       </form>
     </div>
